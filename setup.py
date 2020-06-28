2b27d1a39918db873de255af6f3eca07f4c2b371
Eric Herrera <eric.herrera@edunext.co>
2020-06-28 11:31:40 -0500

    Add setup.py install_requirements definition. Add classifiers. Create requirements folder. create pip_tools and base requirements files. Add Makefile. Add makefile upgrade command. Include requirements files generated using upgrade command. Update openedx.yaml OEPs list.

--- setup.py
+++ setup.py
@@ -17,6 +17,29 @@ def package_data(pkg, root):
     return {pkg: data}
 
 
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
 setup(
     name='animation-xblock',
     version='0.1',
@@ -24,13 +47,17 @@ setup(
     packages=[
         'animation',
     ],
-    install_requires=[
-        'XBlock',
-    ],
+    install_requires=load_requirements('requirements/base.in'),
     entry_points={
         'xblock.v1': [
             'animation = animation:AnimationXBlock',
         ]
     },
     package_data=package_data("animation", "static"),
+    classifiers=[
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.8",
+    ]
 )
